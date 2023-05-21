cdf877a6b556aef9a29134cd40676d76d99dfa4c
Lucas dos Santos <lucas.ffr2014@gmail.com>
2023-05-21 16:37:47 -0300

    Instalação do chakra e outras libs

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,21 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import type { AppProps } from "next/app";
+import { ChakraProvider, extendTheme, Box } from "@chakra-ui/react";
+import { ChainId, ThirdwebProvider } from '@thirdweb-dev/react'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  const theme = extendTheme({
+    fonts: {
+      body: 'roboto, sans-serif'
+    }
+  })
+
+  return (
+    <ThirdwebProvider desiredChainId={ChainId.BinanceSmartChainTestNet}>
+      <ChakraProvider theme={theme}>
+        <Box bg='black' w='100vw' h='100vh'>
+          <Component {...pageProps} />
+        </Box>
+      </ChakraProvider>
+    </ThirdwebProvider>
+  );
 }
